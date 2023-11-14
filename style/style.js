9491fd1be0df5de381058c520bba46153b4414f2
Pedro Rafa <59573148+rafaelxx100@users.noreply.github.com>
2023-11-14 16:23:32 -0300

    Add files via upload

---
+++ style/style.js
@@ -0,0 +1,9 @@
+(function(){
+    var carouselImg = new Carousel({
+    container: '.slideShow',
+    itens: '.projetos-cards',
+    btnPrev: '.prev',
+    btnNext: '.next'
+    })
+    console.log('teste')
+})()
